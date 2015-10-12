8187515321d35d8acc42b5cf2aaf892ce0c8f283
memphistopheles099 <memphistopheles099@yahoo.com>
2015-10-13 02:09:06 +0700

    Delete unneed

--- src/protocol/Header.java
+++
@@ -1,8 +0,0 @@
-package protocol;
-
-public class Header {
-	public static final String LOG_IN = "LOGIN";
-	public static final String LOG_OUT = "LOGOUT";
-	public static final String ONLINE = "ONLINE";
-	public static final String RESPONSE = "RESPONSE";
-}
